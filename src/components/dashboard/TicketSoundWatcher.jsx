f80d185d2d2ee9cbd33a47b2d658c57b6b426b4e
Abu Baker Al-Badry <adambakeralbdoor@gmail.com>
2026-09-16 21:14:06 +0300

    h

--- src/components/dashboard/TicketSoundWatcher.jsx
+++ src/components/dashboard/TicketSoundWatcher.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useCallback } from 'react';
-import api from '../../api/client';
+import api from '../../lib/axios';
 import { playSound } from '../../utils/sound';
 import { useTicketStore, computeUnseen } from '../../store/ticketStore';
 
